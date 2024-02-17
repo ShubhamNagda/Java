591cc2d1e5cbb639944f010dbaeb0f1153944e1c
Shubham Nagda <119925058+ShubhamNagda@users.noreply.github.com>
2024-02-17 16:48:52 +0530

    Add files via upload

---
+++ MainCode/if_else.java
@@ -0,0 +1,16 @@
+public class if_else {
+    public static void main(String[] args) {
+        boolean SeniorCitizen = false;
+        boolean IsAnAdult = false;
+
+        if (SeniorCitizen){
+            System.out.println("Hello "+ SeniorCitizen);
+        }
+        else if (IsAnAdult) {
+            System.out.println("Hello "+IsAnAdult);
+        }
+        else{
+            System.out.println("Hello Child");
+        }
+    }
+}
