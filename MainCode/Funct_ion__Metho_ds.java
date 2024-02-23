b940918d87ad2aae3dfb3aa56c48df349521c75c
Shubham Nagda <119925058+ShubhamNagda@users.noreply.github.com>
2024-02-23 13:07:39 +0530

    Add files via upload

---
+++ MainCode/Funct_ion__Metho_ds.java
@@ -0,0 +1,11 @@
+public class Funct_ion__Metho_ds {
+    public static void main(String[] args) {
+        SayHello();
+
+    }
+// Modifier     return_type  Name_of_Function
+    public static void SayHello(){
+        System.out.println("Hello World");
+    }
+
+}
