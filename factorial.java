e7978d4572a4aae75720929e0a9857db848b51a7
ShubhamNagda <shubhamnagda62@gmail.com>
2026-01-08 20:59:30 +0530

    first commit

---
+++ factorial.java
@@ -0,0 +1,14 @@
+public class factorial {
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println(fact(num));
+    }
+
+    public static int fact(int n) {
+        if (n <= 1) {
+            return 1;
+        } else {
+            return n * fact(n - 1);
+        }
+    }
+}
