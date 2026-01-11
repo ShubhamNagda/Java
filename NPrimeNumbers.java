0d2522beb3d1517233893aef48539a6d455d64cb
ShubhamNagda <shubhamnagda62@gmail.com>
2026-01-11 18:38:28 +0530

    Correction

--- NPrimeNumbers.java
+++ NPrimeNumbers.java
@@ -11,15 +11,25 @@ public class NPrimeNumbers {
             }
             j++;
         }
-        System.out.println();
         scan.close();
     }
 
-    public static boolean isPrime(int n) {
-        if (n == 2 || n == 3) {
-            return true;
-        } else if (n % 2 == 0 || n % 3 == 0) {
+    public static boolean isPrime(int num) {
+        if (num == 1) {
             return false;
+        }
+        if (num == 2) {
+            return true;
+        }
+        if (num % 2 == 0) {
+            return false;
+        } else {
+            for (int i = 3; i < num; i = i + 2) {
+                if (num % i == 0) {
+                    return false;
+                }
+            }
+
         }
         return true;
     }
